eb434e71f407ee57089ecd5550d87d350dbc4acc
hramov <trykhramov@gmail.com>
2023-09-28 17:28:57 +0700

    menu for charts

--- ui/src/views/single_dashboard/components/chart/Chart.vue
+++ ui/src/views/single_dashboard/components/chart/Chart.vue
@@ -8,7 +8,25 @@ const title = props.title;
 
 <template>
   <v-card style="height: 100%; margin: 0; width: 100%">
-    <v-card-title>{{ title }}</v-card-title>
+    <v-card-title>
+      <div style="display:flex; justify-content: space-between; align-items: center">
+        <span>{{ title }}</span>
+        <div style="width: 20%; text-align: right">
+          <v-btn variant="text" icon="mdi-dots-horizontal" size="40"></v-btn>
+          <v-menu location="bottom" offset="0" activator="parent">
+            <v-list>
+              <v-list-item
+                  v-for="(item, index) in ['Опция 1', 'Опция 2']"
+                  :key="index"
+                  :value="index"
+              >
+                <v-list-item-title>{{ item }}</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </div>
+      </div>
+    </v-card-title>
     <v-skeleton-loader v-if="props.loading"></v-skeleton-loader>
     <EChart v-else :type="props.type" :title="props.title" :styles="styles" :options="options" :data="props.data" />
   </v-card>
